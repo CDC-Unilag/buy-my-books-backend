e47647394614692e3a16ccfef599a7fc48bcf81a
Jonathan <farinloyejonathan@gmail.com>
2020-06-15 16:40:19 +0100

    Postgres setup

--- buyMyBooks/settings.py
+++ buyMyBooks/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'buyMyBooks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
