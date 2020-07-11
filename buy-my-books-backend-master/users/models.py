a3769a5605471700a3ae1721d539402d01849af3
Shullyd7 <shullyd7@gmail.com>
2020-07-11 02:32:32 -0700

    added changes to model.py

---
+++ buy-my-books-backend-master/users/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, error_messages={
+            'unique': _('A user with that email already exists.'),
+        }, db_index=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+class Account(models.Model):
+    types = (
+            ('B', 'BUYER' ),('S', 'SELLER')
+            )
+    name = models.Charfield(max_length=15)   
+    username = models.CharField(max_length=15)
+    email = models.EmailField()
+    password = models.CharField(max_length=16)
+    user_type = models.CharField(max_length=12, choices=types)
+
+
